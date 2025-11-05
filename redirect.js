45d23b8dbab49cd66cc4f6cf941de70591eb1eb1
jarvis60 <127525957+jarvis60@users.noreply.github.com>
2025-11-05 16:23:23 -0500

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,8 +1,5 @@
-const _x1 = 'aHR0cHM6Ly93ZWIuYWlzdHJpcC52aXA='; // https://web.aistrip.vip
-const _y2 = atob(_x1);
-
 document.addEventListener('DOMContentLoaded', () => {
   setTimeout(() => {
-    window.location.href = _y2;
+    window.location.href = "https://web.aistrip.vip"; // 或你自己的跳转目标
   }, 3000);
 });
